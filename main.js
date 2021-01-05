c6f6616cb422849f6b24c5fca9f754b887c1c3fb
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2021-01-05 10:31:24 +0100

    new

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-function displayNextImage() {
+
+var images = [], x = -1 , i ;
+for(i = 0 ; i < 5 ; i++){
+    images[i] = i+".jpg";
+}
+
+function next() {
     x = (x === images.length - 1) ? 0 : x + 1;
     document.getElementById("img-id").src = images[x];
 }
 
-function displayPreviousImage() {
-    x = (x <= 0) ? images.length - 1 : x - 1;
-    document.getElementById("img-id").src = images[x];
-}
 
-function startTimer() {
-    setInterval(displayNextImage, 5000 );
+function timer() {
+    setInterval(next, 5000 );
 }
-
-var images = [], x = -1;
-images[0] = "1.jpg";
-images[1] = "2.jpg";
-images[2] = "3.jpg";
